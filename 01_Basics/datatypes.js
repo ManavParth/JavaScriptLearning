49c6626d5a34cdf0ee4759be5c71a9a2529e3332
Mag <155513309+ManavParth@users.noreply.github.com>
2025-12-29 20:07:39 +0530

    Operations in JS

--- 01_Basics/datatypes.js
+++ 01_Basics/datatypes.js
@@ -23,3 +23,32 @@ console.table(values);
 console.log("Is udf1 = udf2 ? -> ", udf1===udf2);
 console.log("typeof null -> ", typeof null);
 console.log("is null an object? -> ", (typeof null)===(typeof values));
+
+/************************* OPERATIONS ****************************/
+
+console.log("\n| OPERATIONS IN JAVASCRIPT |\n");
+
+console.log("STRING CONCATENATION ->");
+console.log(`"7" + 7: ${"7" + 7},
+7 + "7": ${7 + "7"},
+7 + "7" + 7: ${7 + "7" + 7},
+"7" + 7 + 7: ${"7" + 7 + 7},
+7 + 7 + "7": ${7 + 7 + "7"}
+=======================\n`);
+
+// Boolean to integer
+console.log(`BOOLEAN TO INTEGER -->
++true: ${+true}, +false: ${+false}\n`);
+
+// string to integer
+console.log(`STRING TO INTEGER -->
++"": ${+""}, +"mag": ${+"false"}, +" ": ${+" "}\n`);
+
+// increment and decrement operator
+let num = 7;
+console.log("Num: ", num);
+console.log(`INCREMENT OP -> pre: ${++num}, post: ${num++}`);
+console.log(`DECCREMENT OP -> pre: ${--num}, post: ${num--}`);
+
+// pre: it first does the operation then you can use it
+// post: first use it then does the operation
